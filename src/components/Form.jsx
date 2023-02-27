d5fead9d8e53729ec16df71decb7f64c7c8b160d
ohyajk <ohyajk@gmail.com>
2023-02-27 11:36:26 +0530

    add bg

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,7 +13,7 @@ const poster = {
 
 
     return (
-        <form onSubmit={(e) => {e.preventDefault(); addTodo(poster);  e.target.reset(); }} className='my-2 flex flex-col gap-2 md:flex-row md:gap-0 bg-[#3b3b3b] md:rounded-full'>
+        <form onSubmit={(e) => {e.preventDefault(); addTodo(poster);  e.target.reset(); }} className='my-2 flex flex-col gap-2 md:flex-row md:gap-0 md:bg-[#3b3b3b] md:rounded-full'>
             <input onChange={(e)=> setInput(e.target.value)} className='md:w-[300px] py-2 px-4 outline-none rounded-full md:rounded-l-full' placeholder='Add Task Here' type="text" required />
             <input className='bg-emerald-500 py-2 px-4 cursor-pointer rounded-full hover:bg-emerald-500/50' type="submit" value="Add Todo" />
         </form>
